20736506814ff5ed2768a244dae18f8f76766990
Erick Garavito <sangaravi22@gmail.com>
2024-04-21 18:09:45 -0500

    Added initial Spring Boot setup

---
+++ commercial.sales/src/test/java/com/stockwage/commercial/sales/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockwage.commercial.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
