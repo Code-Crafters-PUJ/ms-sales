ae6821b33348882064cd995612cdb39d96252c7d
Erick Garavito <sangaravi22@gmail.com>
2024-04-22 12:40:13 -0500

    Update PaymentMethod.java

---
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/PaymentMethod.java
@@ -0,0 +1,20 @@
+package com.stockwage.commercial.sales.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+public class PaymentMethod {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String method;
+}
