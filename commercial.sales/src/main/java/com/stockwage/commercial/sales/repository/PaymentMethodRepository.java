cc959bd54873951a8344fed44d3996a705b1e13e
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 17:50:59 -0500

    Update PaymentMethodRepository.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/repository/PaymentMethodRepository.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/repository/PaymentMethodRepository.java
@@ -1,5 +1,11 @@
 package com.stockwage.commercial.sales.repository;
 
-public class PaymentMethodRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stockwage.commercial.sales.entity.PaymentMethod;
+
+@Repository
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long>{
     
-}
+} 
