71999348b280d94170757e98a55bd26091cbfadf
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 17:43:15 -0500

    Update relation between Bill and Client

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/Client.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/Client.java
@@ -1,9 +1,16 @@
 package com.stockwage.commercial.sales.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -17,4 +24,9 @@ public class Client {
 
     @NotNull
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "bill", cascade = CascadeType.PERSIST)
+    private List<Bill> bills = new ArrayList<>();
+
 }
