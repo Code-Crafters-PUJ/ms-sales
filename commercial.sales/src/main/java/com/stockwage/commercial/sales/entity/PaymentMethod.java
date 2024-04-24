fb4f0e8a76c08f1f52632607c006df1fd3c5983b
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 17:43:49 -0500

    Update relation between PaymentMethod and Bill

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/PaymentMethod.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/PaymentMethod.java
@@ -1,9 +1,16 @@
 package com.stockwage.commercial.sales.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -17,4 +24,8 @@ public class PaymentMethod {
 
     @NotNull
     private String method;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "bill", cascade = CascadeType.PERSIST)
+    private List<Bill> bills = new ArrayList<>();
 }
