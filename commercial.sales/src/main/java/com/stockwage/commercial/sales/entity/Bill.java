ee7b2d7b9b7abdd427d12dee582143d533fd5f45
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 17:45:31 -0500

    Removing unused imports

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/Bill.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/Bill.java
@@ -1,21 +1,13 @@
 package com.stockwage.commercial.sales.entity;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
