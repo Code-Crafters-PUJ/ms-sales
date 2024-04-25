25d30602fae3fa4be2f4d01f186e35f4f13d7af2
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 23:33:42 -0500

    Update ClientServiceImpl.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/Client/ClientServiceImpl.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/Client/ClientServiceImpl.java
@@ -1,5 +1,52 @@
 package com.stockwage.commercial.sales.service.Client;
 
-public class ClientServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.stockwage.commercial.sales.entity.Client;
+import com.stockwage.commercial.sales.repository.ClientRepository;
+
+@Service
+public class ClientServiceImpl implements ClientService{
+
+    @Autowired
+    private ClientRepository clientRepository;
+    @Override
+    public Client getById(Long id) {
+        return clientRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Client getByName(String name) {
+        return clientRepository.findByName(name);
+    }
+
+    @Override
+    public Client save(Client client) {
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        try {
+            clientRepository.deleteById(id);
+            return true;
+        } catch (EmptyResultDataAccessException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Client update(Client client) {
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public List<Client> getAll() {
+        return clientRepository.findAll();
+    }
     
 }
