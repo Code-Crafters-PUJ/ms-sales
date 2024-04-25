bc3521aad1d7c248ca391ab7a40928423c3d39de
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 23:35:55 -0500

    Update PaymentMethodService.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/paymentmethod/PaymentMethodService.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/paymentmethod/PaymentMethodService.java
@@ -1,5 +1,14 @@
 package com.stockwage.commercial.sales.service.paymentmethod;
 
+import java.util.List;
+
+import com.stockwage.commercial.sales.entity.PaymentMethod;
+
 public interface PaymentMethodService {
-    
+    PaymentMethod getById(Long id);
+    PaymentMethod getByMethod(String method);
+    PaymentMethod save(PaymentMethod paymentMethod);
+    boolean delete(Long id);
+    PaymentMethod update(PaymentMethod paymentMethod);
+    List<PaymentMethod> getAll();
 }
