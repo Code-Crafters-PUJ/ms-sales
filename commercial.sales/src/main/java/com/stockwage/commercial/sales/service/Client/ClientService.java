82de41e6c65b536298a03845d3d8ff0b91d5389e
Erick Garavito <sangaravi22@gmail.com>
2024-04-24 23:24:36 -0500

    Update ClientService.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/Client/ClientService.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/Client/ClientService.java
@@ -1,5 +1,14 @@
 package com.stockwage.commercial.sales.service.Client;
 
-public interface ClientService {
+import java.util.List;
 
+import com.stockwage.commercial.sales.entity.Client;
+
+public interface ClientService {
+    Client getById(Integer id);
+    Client getByName(String name);
+    Client save(Client client);
+    void delete(Integer id);
+    Client update(Client client);
+    List<Client> getAll();
 }
