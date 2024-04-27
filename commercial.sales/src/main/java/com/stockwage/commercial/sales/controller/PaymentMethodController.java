128e933e27cc2dca86413efb45cae39d3abd5022
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 13:49:10 -0500

    Update PaymentMethodController.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/controller/PaymentMethodController.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/controller/PaymentMethodController.java
@@ -1,5 +1,91 @@
 package com.stockwage.commercial.sales.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.stockwage.commercial.sales.entity.PaymentMethod;
+import com.stockwage.commercial.sales.service.paymentmethod.PaymentMethodService;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/paymentMethod")
+@Api(tags = "Payment Method Management", description = "Endpoints for managing payment methods")
 public class PaymentMethodController {
 
+    @Autowired
+    private PaymentMethodService paymentMethodService;
+
+    @GetMapping("/all")
+    @Operation(summary = "Get all payment methods")
+    @ApiResponse(responseCode = "200", description = "Payment methods retrieved successfully")
+    @ApiResponse(responseCode = "404", description = "No payment methods found")
+    public ResponseEntity<List<PaymentMethod>> getAllPaymentMethods() {
+        List<PaymentMethod> paymentMethods = paymentMethodService.getAll();
+        if (paymentMethods.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(paymentMethods, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    @Operation(description = "Add a new payment method")
+    @ApiResponse(responseCode = "201", description = "Payment method added successfully")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    public ResponseEntity<PaymentMethod> addPaymentMethod(@RequestBody PaymentMethod paymentMethod) {
+        if (paymentMethod == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        PaymentMethod newPaymentMethod = paymentMethodService.save(paymentMethod);
+        return new ResponseEntity<>(newPaymentMethod, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get")
+    @Operation(summary = "Get a payment method by ID")
+    @ApiResponse(responseCode = "200", description = "Payment method retrieved successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid ID supplied")
+    @ApiResponse(responseCode = "404", description = "Payment method not found")
+    public ResponseEntity<PaymentMethod> getPaymentMethodById(@RequestParam Long id) {
+        Optional<PaymentMethod> paymentMethod = paymentMethodService.getById(id);
+        return paymentMethod.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/update/{id}")
+    @Operation(summary = "Update an existing payment method")
+    @ApiResponse(responseCode = "200", description = "Payment method updated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid ID supplied or Bad request")
+    @ApiResponse(responseCode = "404", description = "Payment method not found")
+    public ResponseEntity<PaymentMethod> updatePaymentMethod(@PathVariable Long id, @RequestBody PaymentMethod updatedPaymentMethod) {
+        if (id == null || updatedPaymentMethod == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<PaymentMethod> existingPaymentMethodOptional = paymentMethodService.getById(id);
+        if (existingPaymentMethodOptional.isPresent()) {
+            PaymentMethod existingPaymentMethod = existingPaymentMethodOptional.get();
+            existingPaymentMethod.setMethod(updatedPaymentMethod.getMethod());
+            PaymentMethod savedPaymentMethod = paymentMethodService.update(existingPaymentMethod);
+            return new ResponseEntity<>(savedPaymentMethod, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    @Operation(summary = "Delete a payment method by ID")
+    @ApiResponse(responseCode = "200", description = "Payment method deleted successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid ID supplied")
+    @ApiResponse(responseCode = "404", description = "Payment method not found")
+    public ResponseEntity<Void> deletePaymentMethod(@RequestParam Long id) {
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        boolean deleted = paymentMethodService.delete(id);
+        return deleted ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
