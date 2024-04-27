5f420ced240b771037b49a99106a6c5a106b30ec
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 18:00:34 -0500

    Update BillDTO.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BillDTO.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BillDTO.java
@@ -1,6 +1,43 @@
 package com.stockwage.commercial.sales.dto;
 
-public class BillDTO {
+import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class BillDTO {
+    
+    private Long id;
+
+    private String type;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    private String description;
+
+    private String seller;
+
+    private Long branchId;
+
+    private String email;
+
+    private Double subtotal;
+
+    private Double discount;
+
+    private Double taxes;
+
+    private boolean aiu;
+
+    private boolean withholdingTax;
+
+    private boolean chargeTax;
+
+    private Long clientId;
+
+    private Long paymentMethodId;
     
 }
