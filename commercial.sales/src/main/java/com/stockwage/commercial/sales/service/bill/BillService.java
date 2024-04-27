810253c64afcee55fd2440be3ec857814f4b7f26
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 17:09:54 -0500

    Update BillService.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/bill/BillService.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/bill/BillService.java
@@ -1,5 +1,15 @@
 package com.stockwage.commercial.sales.service.bill;
 
-public class BillService {
-    
+import java.util.List;
+import java.util.Optional;
+
+import com.stockwage.commercial.sales.entity.Bill;
+
+public interface BillService {
+    Optional<Bill> getById(Long id);
+    Optional<Bill> getByName(String name);
+    Bill save(Bill bill);
+    boolean delete(Long id);
+    Bill update(Bill bill);
+    List<Bill> getAll();
 }
