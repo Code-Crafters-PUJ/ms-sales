5c89effcef4149fba40827986af2ef1f4560dcdf
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 14:59:19 -0500

    Update ProductService.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductService.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductService.java
@@ -1,5 +1,14 @@
 package com.stockwage.commercial.sales.service.product;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.stockwage.commercial.sales.entity.Product;
+
 public interface ProductService {
-    
+    Optional<Product> getById(Long id);
+    Product save(Product product);
+    boolean delete(Long id);
+    Product update(Product product);
+    List<Product> getAll();
 }
