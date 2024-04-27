08ca0884d1113b30055f4f9d8f974ff51f218598
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 14:23:32 -0500

    Update AlreadyExistsException.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/exception/AlreadyExistsException.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/exception/AlreadyExistsException.java
@@ -1,5 +1,7 @@
 package com.stockwage.commercial.sales.exception;
 
-public class AlreadyExistsException {
-    
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
 }
