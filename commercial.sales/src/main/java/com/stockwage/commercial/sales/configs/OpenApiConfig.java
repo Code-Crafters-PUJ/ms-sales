98d479dc683d0e0fcfb67e3822f6df3d54169c26
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 13:48:17 -0500

    Update OpenApiConfig.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/OpenApiConfig.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/OpenApiConfig.java
@@ -1,5 +1,70 @@
 package com.stockwage.commercial.sales.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Commercial Sales API",
+        version = "1.0",
+        description = "API for managing commercial sales"
+    )
+)
 public class OpenApiConfig {
 
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        ApiResponse badRequest = new ApiResponse().content(
+                new Content().addMediaType("application/json",
+                        new io.swagger.v3.oas.models.media.MediaType().addExamples("default",
+                                new Example().value("{\"code\" : 400, \"status\" : \"Bad Request\", \"Message\" : \"Bad Request\"}"))));
+        ApiResponse internalServerError = new ApiResponse().content(
+                new Content().addMediaType("application/json",
+                        new io.swagger.v3.oas.models.media.MediaType().addExamples("default",
+                                new Example().value("{\"code\" : 500, \"status\" : \"internalServerError\", \"Message\" : \"internalServerError\"}"))));
+        ApiResponse successfulResponse = new ApiResponse().content(
+                new Content().addMediaType("application/json",
+                        new io.swagger.v3.oas.models.media.MediaType().addExamples("default",
+                                new Example().value("{\"name\":\"string\",\"surname\":\"string\",\"age\":0}"))));
+        ApiResponse notFoundResponse = new ApiResponse().content(
+                new Content().addMediaType("application/json",
+                        new io.swagger.v3.oas.models.media.MediaType().addExamples("default",
+                                new Example().value("{\"code\" : 404, \"status\" : \"Not Found\", \"Message\" : \"Resource not found\"}"))));
+
+        Components components = new Components();
+        components.addResponses("badRequest",badRequest);
+        components.addResponses("internalServerError",internalServerError);
+        components.addResponses("successfulResponse",successfulResponse);
+        components.addResponses("notFoundResponse", notFoundResponse);
+
+        // Agrega una respuesta "OK"
+        ApiResponse okResponse = new ApiResponse().content(
+                new Content().addMediaType("application/json",
+                        new io.swagger.v3.oas.models.media.MediaType().addExamples("default",
+                                new Example().value("{\"code\" : 200, \"status\" : \"OK\", \"Message\" : \"OK\"}"))));
+        components.addResponses("okResponse", okResponse);
+
+        return new OpenAPI().components(components);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.stockwage.commercial.sales.controller"))
+                .build();
+    }
 }
