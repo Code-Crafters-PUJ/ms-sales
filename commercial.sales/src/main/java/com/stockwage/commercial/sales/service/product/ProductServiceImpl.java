0db4e0ab3434a2a212148703d55dc1833dc072a4
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 15:01:50 -0500

    Update ProductServiceImpl.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductServiceImpl.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductServiceImpl.java
@@ -1,5 +1,50 @@
 package com.stockwage.commercial.sales.service.product;
 
-public class ProductServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stockwage.commercial.sales.entity.Product;
+import com.stockwage.commercial.sales.repository.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Optional<Product> getById(Long id) {
+        return productRepository.findById(id);
+    }
     
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+    
+    @Override
+    public boolean delete(Long id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()) {
+            productRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    @Override
+    public Product update(Product product) {
+        return productRepository.save(product);
+    }
+    
+    @Override
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
 }
