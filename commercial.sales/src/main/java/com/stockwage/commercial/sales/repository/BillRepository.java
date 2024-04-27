51c1fc47f9ca74c78c7893702dd851424462fe77
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 16:50:48 -0500

    Update BillRepository.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/repository/BillRepository.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/repository/BillRepository.java
@@ -1,11 +1,12 @@
 package com.stockwage.commercial.sales.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.stockwage.commercial.sales.entity.Bill;
 
-@Repository
-public interface BillRepository extends JpaRepository<Bill, Long>{
-    
-} 
+public interface BillRepository extends JpaRepository<Bill, Long> {
+        @Query(value = "SELECT * FROM bill WHERE branch_id = ?1", nativeQuery = true)
+    List<Bill> findByBranchId(Long branchId);
+}
