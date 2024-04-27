11711c9733d67cd4867a6e5fd207f9e8d655b1c1
Erick Garavito <sangaravi22@gmail.com>
2024-04-27 14:39:59 -0500

    Update Product.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/Product.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/entity/Product.java
@@ -1,5 +1,37 @@
 package com.stockwage.commercial.sales.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
 public class Product {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private Integer quantity;
+
+    @NotNull
+    private Double costPrice;
+
+    @NotNull
+    private Double salePrice;
+
+    private Integer discount;
+
+    @NotNull
+    private Long categoryId;
 }
