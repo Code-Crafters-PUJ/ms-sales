943b99d7157005735d2bad512266732e24fb3025
Erick Garavito <sangaravi22@gmail.com>
2024-04-28 01:04:20 -0500

    Update BillProductDTO.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BillProductDTO.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BillProductDTO.java
@@ -1,5 +1,10 @@
 package com.stockwage.commercial.sales.dto;
 
+import lombok.Data;
+
+@Data
 public class BillProductDTO {
-    
+    private Long product_id;
+    private Long bill_id;
+    private Integer quantity;
 }
