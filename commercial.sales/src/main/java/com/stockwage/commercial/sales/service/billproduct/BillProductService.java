fcd2b2679f15611cec4de4f6f6177cd1dbb12d9f
Erick Garavito <sangaravi22@gmail.com>
2024-04-28 01:06:15 -0500

    Update BillProductService.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/billproduct/BillProductService.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/billproduct/BillProductService.java
@@ -1,5 +1,11 @@
 package com.stockwage.commercial.sales.service.billproduct;
 
-public class BillProductService {
-    
+import java.util.List;
+
+import com.stockwage.commercial.sales.dto.BillProductDTO;
+import com.stockwage.commercial.sales.entity.Bill;
+
+public interface BillProductService {
+    Bill save(BillProductDTO BillProductDTO);
+    List<BillProductDTO> getAll();
 }
