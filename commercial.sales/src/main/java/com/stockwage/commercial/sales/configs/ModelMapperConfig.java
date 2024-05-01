62cff616b56e5026d0f64ce7c1a6b90470b8a36c
Erick Garavito <sangaravi22@gmail.com>
2024-04-30 23:20:48 -0500

    Update ModelMapperConfig.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/ModelMapperConfig.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/ModelMapperConfig.java
@@ -1,5 +1,14 @@
 package com.stockwage.commercial.sales.configs;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
-    
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
