f6fff83e4dfe7cbb1ea88c284fca75d0ef4011be
Erick Garavito <sangaravi22@gmail.com>
2024-04-30 23:21:11 -0500

    Update ClientDTO.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/ClientDTO.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/ClientDTO.java
@@ -1,5 +1,16 @@
 package com.stockwage.commercial.sales.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ClientDTO {
-    
+    private String name;
 }
