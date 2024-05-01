6bd8a15765dcfcec8e9c2ea42ab3ae054e38ce7a
Erick Garavito <sangaravi22@gmail.com>
2024-05-01 10:16:45 -0500

    Update BillProductDTO.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BillProductDTO.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BillProductDTO.java
@@ -1,18 +1,12 @@
 package com.stockwage.commercial.sales.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class BillProductDTO {
     private Long product_id;
     private Long bill_id;
-    @NotNull
     private Integer quantity;
-
-    @NotNull
     private Double unitPrice;
-
-    @NotNull
     private Integer discountPercentage;
 }
