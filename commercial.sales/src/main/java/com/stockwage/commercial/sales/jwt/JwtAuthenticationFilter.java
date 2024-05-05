4651686832c562fea25f1d8c5772dbb80b557e8c
Erick Garavito <sangaravi22@gmail.com>
2024-05-04 22:29:08 -0500

    Implemented JwtAuthenticationFilter class for JWT token authentication.

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/jwt/JwtAuthenticationFilter.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,60 @@
 package com.stockwage.commercial.sales.jwt;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
-public class JwtAuthenticationFilter  {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Override
+protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+    String token = extractTokenFromHeader(request.getHeader("Authorization"));
+    if (token != null) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String role = (String) claims.get("Role");
+            SecurityContextHolder.getContext().setAuthentication(
+                    new UsernamePasswordAuthenticationToken(null, null,
+                            Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role))));
+        } catch (Exception e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write("Error al validar el token: " + e.getMessage());
+            return;
+        }
+    } else {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write("Token is required");
+        return;
+    }
+    filterChain.doFilter(request, response);
+}
+
+
+    private String extractTokenFromHeader(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        return null;
+    }
 }
