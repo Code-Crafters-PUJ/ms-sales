ae23c139c4e8a607d0191b2c0c2f7ea929778b1e
Erick Garavito <sangaravi22@gmail.com>
2024-05-04 22:29:49 -0500

    Implemented SecurityConfig class for configuring JWT authentication.

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/SecurityConfig.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/SecurityConfig.java
@@ -1,5 +1,41 @@
 package com.stockwage.commercial.sales.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.stockwage.commercial.sales.jwt.JwtAuthEntryPoint;
+import com.stockwage.commercial.sales.jwt.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    
+    @Autowired
+    private JwtAuthEntryPoint jwtAuthEntryPoint;
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+        .csrf(AbstractHttpConfigurer::disable)
+        .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+        .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint));
+    http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    
+         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
 }
