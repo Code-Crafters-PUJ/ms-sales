6b180f9608135a024365a65d04b0053ddf3600ed
Erick Garavito <sangaravi22@gmail.com>
2024-05-12 11:57:45 -0500

    Create BranchProductDTO.java

---
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BranchProductDTO.java
@@ -0,0 +1,8 @@
+package com.stockwage.commercial.sales.dto;
+
+public class BranchProductDTO {
+    private Long branchId;
+    private Long productId;
+    private Integer quantity;
+    private Integer discount = 0;
+}
