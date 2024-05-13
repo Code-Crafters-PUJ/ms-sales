0c3cec72017f0dc78ce52b4fc8d0fe98b484a14d
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 03:42:35 -0500

    Add branch-product-quantity-update-queue

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/producer/branchproduct/BranchProductQuantityUpdateProducer.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/producer/branchproduct/BranchProductQuantityUpdateProducer.java
@@ -1,5 +1,20 @@
 package com.stockwage.commercial.sales.rabbitmq.producer.branchproduct;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BranchProductQuantityUpdateProducer {
     
+    @Autowired
+    AmqpTemplate amqpTemplate;
+
+    public void sendBranchProductQuantityUpdateMessage(Long productId, Long branchId, Integer newQuantity) {
+        String queueName = "branch-product-quantity-update-queue";
+
+        String message = productId + "," + branchId + "," + newQuantity;
+        amqpTemplate.convertAndSend(queueName, message);
+        System.out.println("Mensaje de actualización de cantidad de producto de sucursal enviado: " + message);
+    }
 }
