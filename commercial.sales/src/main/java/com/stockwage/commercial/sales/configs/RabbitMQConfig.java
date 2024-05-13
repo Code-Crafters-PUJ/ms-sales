3fc38a494b7bf2072a3739b7abf53accddcbca86
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 02:32:42 -0500

    Update RabbitMQConfig.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/RabbitMQConfig.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/configs/RabbitMQConfig.java
@@ -1,5 +1,20 @@
 package com.stockwage.commercial.sales.configs;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
-    
+
+    @Bean
+    public Queue createUpdateProductQueue(){
+        return new Queue("create-update-product-queue", true);
+    }
+
+    @Bean
+    public Queue deleteProductQueue() {
+        return new Queue("delete-product-queue", true);
+    }
+
 }
