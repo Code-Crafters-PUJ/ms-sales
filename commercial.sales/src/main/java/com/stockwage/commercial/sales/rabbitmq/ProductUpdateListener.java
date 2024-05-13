f4f8e7b00dc11ad498b32df705f58b4e928a4413
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 02:10:28 -0500

    Update ProductUpdateListener.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/ProductUpdateListener.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/ProductUpdateListener.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stockwage.commercial.sales.dto.ProductDTO;
 import com.stockwage.commercial.sales.entity.Product;
 import com.stockwage.commercial.sales.service.product.ProductService;
 
@@ -18,29 +20,21 @@ public class ProductUpdateListener {
 
     @RabbitListener(queues = "update-product-queue")
     public void processProductUpdate(@Payload String message) {
-        String[] parts = message.split(",");
-        Long productId = Long.parseLong(parts[0]);
-        String productName = parts[1];
-        String productDescription = parts[2];
-        Integer productQuantity = Integer.parseInt(parts[3]);
-        Double productCostPrice = Double.parseDouble(parts[4]);
-        Double productSalePrice = Double.parseDouble(parts[5]);
-        Integer productDiscount = Integer.parseInt(parts[6]);
-        Integer productCategoryId = Integer.parseInt(parts[7]);
 
-        Optional<Product> existingProduct = productService.getById(productId);
-        if (existingProduct.isPresent()) {
-            Product product = new Product();
-            product.setId(productId);
-            product.setName(productName);
-            product.setDescription(productDescription);
-            product.setQuantity(productQuantity);
-            product.setCostPrice(productCostPrice);
-            product.setSalePrice(productSalePrice);
-            product.setDiscount(productDiscount);
-            product.setCategoryId(productCategoryId);
-            Product updatedProduct = productService.update(product);
-            System.out.println(updatedProduct.toString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        ProductDTO productDTO = null;
+        try {
+            productDTO = objectMapper.readValue(message, ProductDTO.class);
+            Optional<Product> existingProduct = productService.getById(productDTO.getId());
+            if (existingProduct.isPresent()) {
+                Product product = productService.DtoToEntity(productDTO);
+                productService.update(product);
+            } else {
+                System.err.println("The product with ID " + productDTO.getId() + " does not exist.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error processing message: " + message);
         }
     }
 }
