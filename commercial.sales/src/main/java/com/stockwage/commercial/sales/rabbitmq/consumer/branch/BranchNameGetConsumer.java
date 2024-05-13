48c110f6de032a957d133853ba1ae5e53d8f6980
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 04:04:15 -0500

    Update BranchNameGetConsumer.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/consumer/branch/BranchNameGetConsumer.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/consumer/branch/BranchNameGetConsumer.java
@@ -1,5 +1,16 @@
 package com.stockwage.commercial.sales.rabbitmq.consumer.branch;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BranchNameGetConsumer {
     
+    @RabbitListener(queues = "branch-name-get-queue")
+    public String processBranchProductUpdate(@Payload String branchName) {
+        System.out.println("Branch name received: " + branchName);
+        return branchName;
+    }
+
 }
