7770b66b471e2021991b3c91105c648a975cf5cc
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 02:32:37 -0500

    Update ProductDeleteListener.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/listener/ProductDeleteListener.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/listener/ProductDeleteListener.java
@@ -1,5 +1,30 @@
 package com.stockwage.commercial.sales.rabbitmq.listener;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.stockwage.commercial.sales.service.product.ProductService;
+
+@Component
 public class ProductDeleteListener {
-    
+
+    @Autowired
+    ProductService productService;
+
+    @RabbitListener(queues = "delete-product-queue")
+    public void processProductDelete(@Payload String stringProductId) {
+        Long productId = Long.valueOf(stringProductId);
+        System.out.println("Receive delete message: " + productId);
+        try {
+            // Delete the product with the provided ID
+            productService.delete(productId);
+            System.out.println("Product with ID " + productId + " deleted.");
+        } catch (Exception e) {
+            // Handle errors when deleting products
+            e.printStackTrace();
+            System.err.println("Error deleting product with ID " + productId);
+        }
+    }
 }
