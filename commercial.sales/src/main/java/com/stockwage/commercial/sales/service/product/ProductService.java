50c411def2ad7dc6a0e689bb402efb49a50ac7b7
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 02:10:20 -0500

    Add DTo to Entity service

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductService.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductService.java
@@ -3,9 +3,11 @@ package com.stockwage.commercial.sales.service.product;
 import java.util.List;
 import java.util.Optional;
 
+import com.stockwage.commercial.sales.dto.ProductDTO;
 import com.stockwage.commercial.sales.entity.Product;
 
 public interface ProductService {
+    public Product DtoToEntity(ProductDTO productDTO);
     Optional<Product> getById(Long id);
     Product save(Product product);
     boolean delete(Long id);
--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductServiceImpl.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/product/ProductServiceImpl.java
@@ -3,9 +3,11 @@ package com.stockwage.commercial.sales.service.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.stockwage.commercial.sales.dto.ProductDTO;
 import com.stockwage.commercial.sales.entity.Product;
 import com.stockwage.commercial.sales.repository.ProductRepository;
 
@@ -16,6 +18,13 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Product DtoToEntity(ProductDTO productDTO) {
+        return modelMapper.map(productDTO, Product.class);
+    }
 
     @Override
     public Optional<Product> getById(Long id) {
