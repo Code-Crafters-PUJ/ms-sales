50e7d2dc593287d70a7458ae99a208a7372233f7
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 02:53:08 -0500

    Add CRUD methods

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/branchproduct/BranchProductService.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/branchproduct/BranchProductService.java
@@ -1,5 +1,17 @@
 package com.stockwage.commercial.sales.service.branchproduct;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.stockwage.commercial.sales.dto.BranchProductDTO;
+import com.stockwage.commercial.sales.entity.BranchProduct;
+
 public interface BranchProductService {
+    public BranchProduct DtoToEntity(BranchProductDTO branchProductDTO);
+    Optional<BranchProduct> getById(Long id);
+    BranchProduct save(BranchProduct branchProduct);
+    boolean delete(Long id);
+    BranchProduct update(BranchProduct branchProduct);
+    List<BranchProduct> getAll();
     Boolean updateQuantity(Long productId, Long branchId, Integer newQuantity);
 }
--- commercial.sales/src/main/java/com/stockwage/commercial/sales/service/branchproduct/BranchProductServiceImpl.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/service/branchproduct/BranchProductServiceImpl.java
@@ -1,8 +1,13 @@
 package com.stockwage.commercial.sales.service.branchproduct;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.stockwage.commercial.sales.dto.BranchProductDTO;
 import com.stockwage.commercial.sales.entity.BranchProduct;
 import com.stockwage.commercial.sales.repository.BranchProductRepository;
 
@@ -10,8 +15,16 @@ import com.stockwage.commercial.sales.repository.BranchProductRepository;
 @Service
 public class BranchProductServiceImpl implements BranchProductService{
     
-        @Autowired
+    @Autowired
     private BranchProductRepository branchProductRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public BranchProduct DtoToEntity(BranchProductDTO branchProductDTO) {
+        return modelMapper.map(branchProductDTO, BranchProduct.class);
+    }
 
     public Boolean updateQuantity(Long productId, Long branchId, Integer newQuantity) {
         BranchProduct branchProduct = branchProductRepository.findByProductIdAndBranchId(productId, branchId);
@@ -21,6 +34,38 @@ public class BranchProductServiceImpl implements BranchProductService{
             return true;
         } 
         return false;
+    }
+
+    @Override
+    public Optional<BranchProduct> getById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    @Override
+    public BranchProduct save(BranchProduct branchProduct) {
+        return branchProductRepository.save(branchProduct);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        Optional<BranchProduct> branchProductOptional = branchProductRepository.findById(id);
+        if (branchProductOptional.isPresent()) {
+            branchProductRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public BranchProduct update(BranchProduct branchProduct) {
+        return branchProductRepository.save(branchProduct);
+    }
+
+    @Override
+    public List<BranchProduct> getAll() {
+       return branchProductRepository.findAll();
     }
 
 }
