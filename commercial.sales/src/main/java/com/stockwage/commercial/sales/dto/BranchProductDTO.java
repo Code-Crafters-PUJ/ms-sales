4b9b55fe393eda9b52317835ca7e878abd787441
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 02:36:15 -0500

    Update BranchProductDTO.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BranchProductDTO.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/BranchProductDTO.java
@@ -1,5 +1,9 @@
 package com.stockwage.commercial.sales.dto;
 
 public class BranchProductDTO {
-
+    private Long id;
+    private Long branchId;
+    private Long productId;
+    private Integer discount;
+    private Integer quantity;
 }
