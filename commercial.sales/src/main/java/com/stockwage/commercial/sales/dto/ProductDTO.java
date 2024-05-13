3ae760ede70be66810b4dd6c3c7ccc2a3b70b9e2
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 00:01:01 -0500

    Update ProductDTO.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/ProductDTO.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/dto/ProductDTO.java
@@ -1,5 +1,12 @@
 package com.stockwage.commercial.sales.dto;
+import lombok.Data;
 
+@Data
 public class ProductDTO {
-    
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double salePrice;
+    private Integer categoryId;
 }
