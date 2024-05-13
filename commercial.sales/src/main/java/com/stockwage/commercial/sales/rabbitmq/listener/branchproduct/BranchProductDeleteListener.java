64e3642a51c10068cd1b2b353f395c3135f7f33c
Erick Garavito <sangaravi22@gmail.com>
2024-05-13 03:15:49 -0500

    Update BranchProductDeleteListener.java

--- commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/listener/branchproduct/BranchProductDeleteListener.java
+++ commercial.sales/src/main/java/com/stockwage/commercial/sales/rabbitmq/listener/branchproduct/BranchProductDeleteListener.java
@@ -1,5 +1,29 @@
 package com.stockwage.commercial.sales.rabbitmq.listener.branchproduct;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.stockwage.commercial.sales.service.branchproduct.BranchProductService;
+
+@Component
 public class BranchProductDeleteListener {
-    
+    @Autowired
+    BranchProductService branchProductService;
+
+    @RabbitListener(queues = "delete-branch-product-queue")
+    public void processBranchProductDelete(@Payload String stringBranchProductId) {
+        Long branchProductId = Long.valueOf(stringBranchProductId);
+        System.out.println("Receive delete message: " + branchProductId);
+        try {
+            // Delete the branch product with the provided ID
+            branchProductService.delete(branchProductId);
+            System.out.println("Branch product with ID " + branchProductId + " deleted.");
+        } catch (Exception e) {
+            // Handle errors when deleting branch products
+            e.printStackTrace();
+            System.err.println("Error deleting branch product with ID " + branchProductId);
+        }
+    }
 }
